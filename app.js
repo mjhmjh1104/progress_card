2f92a473a91788996212b6ebf4282515814c04df
mjhmjh1104 <mjhmjh1104@naver.com>
2017-07-22 16:59:51 +0900

    updated

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-console.log(req);
-console.log(res);
+//console.log(res);
 
 app.get('/', function(req, res) {
   res.send('Welcome!');
+  console.log(req);
 });
 
 app.listen(3000, function() {
