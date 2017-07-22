2095d09cb10dac2464eec49cba5ac46464892dbb
mjhmjh1104 <mjhmjh1104@naver.com>
2017-07-22 17:43:41 +0900

    updated

--- app.js
+++ app.js
@@ -9,22 +9,29 @@ var data = {count : 0};
 app.get('/', function(req, res) {
   data.count++;
   res.render('main', data);
+  console.log('Boop! Someone enters :\n/');
 });
 
 app.get('/reset', function(req, res) {
   data.count = 0;
   res.render('main', data);
+  console.log('Boop! Someone enters :\n/reset');
 });
 
 app.get('/set/count', function(req, res) {
   if (req.query.count)
     data.count = req.query.count;
   res.render('main', data);
+  if (req.query.count)
+    console.log('Boop! Someone enters :\n/set/count?count=' + req.query.count);
+  else
+    console.log('Boop! Someone enters :\n/set/count')
 });
 
 app.get('/set/:num', function(req,res) {
   data.count = req.params.num;
   res.render('main', data);
+  console.log('Boop! Someone enters :\n/set/' + req.params.num);
 });
 
 app.listen(3000, function() {
