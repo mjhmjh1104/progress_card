06c4789f9cb8461e62bcdcd31e6d4752e0697f35
mjhmjh1104 <mjhmjh1104@naver.com>
2017-07-22 17:17:11 +0900

    updated

--- app.js
+++ app.js
@@ -3,9 +3,29 @@ var path = require('path');
 var app = express();
 
 app.set("view engine", 'ejs');
+
+var data = {count : 0};
+
 app.get('/', function(req, res) {
-  res.render('main.ejs');
+  data.count++;
+  res.render('main', data);
 });
+
+app.get('/reset', function(req, res) {
+  data.count = 0;
+  res.render('main', data);
+});
+
+app.get('/set/count', function(req, res) {
+  if (req,query.count)
+    data.count = req.query.count;
+  res.render('main', data);
+});
+
+app.get('/set/:num', function(req,res) {
+  data.count = req.params.num;
+  res.render('main', data);
+})
 
 app.listen(3000, function() {
   console.log('Server On!');
